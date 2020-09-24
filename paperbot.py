68af3ea6e587b229824f798705e6d26580135aea
gconnellbass <georgia.connell-bass@altran.com>
2020-09-24 16:33:16 +0100

    Wrote random class for bot so it choses a move at random

--- paperbot.py
+++ paperbot.py
@@ -1,6 +1,12 @@
-class PaperBot:
-    def __init__(self):
-        pass
+# class PaperBot:
+#     def __init__(self):
+#         pass
+#
+#     def make_move(self, gamestate):
+#         return 'P'
 
-    def make_move(self, gamestate):
-        return 'P'
+class RandomBot:
+    def __init__(self):
+        available_moves = ['R', 'P', 'S', 'W', 'D']
+    def make_random_move(self, available_moves):
+        return available_moves.choice()
