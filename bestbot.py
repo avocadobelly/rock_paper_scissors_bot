2020514e31879adea890b3642fc1eeb9f1079fc9
gconnellbass <georgia.connell-bass@altran.com>
2020-09-25 12:31:22 +0100

    Added dynamite tracker so bot will not use 'D' more than 100 times

--- bestbot.py
+++ bestbot.py
@@ -1,8 +1,19 @@
 import random
 
 class RandomBot:
+    global random
     def __init__(self):
         self.available_moves = ['R', 'P', 'S', 'W', 'D']
+        self.dynamite_supply = 100
 
     def make_move(self, gamestate):
-        return random.choice(self.available_moves)
+        choice = random.choice(self.available_moves)
+        if choice == 'D':
+            self.dynamite_supply_tracker()
+        return choice
+
+    def dynamite_supply_tracker(self):
+        if self.dynamite_supply > 0:
+            self.dynamite_supply = self.dynamite_supply - 1
+        if self.dynamite_supply == 0:
+            self.available_moves.remove('D')
