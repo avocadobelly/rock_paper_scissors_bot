7afe65d74c6918c4bf36cd8ba50e74bba0c2ab17
gconnellbass <georgia.connell-bass@altran.com>
2020-09-28 11:16:25 +0100

    Changed opponent_used_moves to a dictionary that will keep track of how many times each move is used.

--- bestbot.py
+++ bestbot.py
@@ -5,25 +5,30 @@ class RandomBot:
     def __init__(self):
         self.available_moves = ['R', 'P', 'S', 'D']
         self.dynamite_supply = 100
-        self.opponents_used_dynamite = 0
-        self.opponent_used_moves = []
+        # self.opponents_used_dynamite = 0
+        self.opponent_used_moves = {}
+        self.opponent_used_moves['R'] = 0
+        self.opponent_used_moves['P'] = 0
+        self.opponent_used_moves['S'] = 0
+        self.opponent_used_moves['D'] = 0
+        self.opponent_used_moves['W'] = 0
+
 
     def make_move(self, gamestate):
-        #check if opponent has used all dynamite:
 
         rounds = gamestate['rounds']
 
         if rounds:
             opponents_previous_move = rounds[- 1]['p2']
             if opponents_previous_move == 'D':
-                self.opponent_dynamite_tracker()
-            self.opponent_used_moves.append(opponents_previous_move)
+                self.opponent_used_moves['D'] += 1
+
         # Only add water to available moves once opponent has used their first dynamite
-        if self.opponents_used_dynamite == 1:
+        if self.opponent_used_moves['D'] == 1:
             self.available_moves.append('W')
 
-        #If opponent has used all their dynamite, remove water from available moves
-        if self.opponents_used_dynamite == 100 and 'W' in self.available_moves:
+        # If opponent has used all their dynamite, remove water from available moves
+        if self.opponent_used_moves['D'] == 100 and 'W' in self.available_moves:
             self.available_moves.remove('W')
 
         choice = random.choice(self.available_moves)
@@ -38,7 +43,10 @@ class RandomBot:
         if self.dynamite_supply == 0:
             self.available_moves.remove('D')
         return self.available_moves
+    #
+    # def opponent_dynamite_tracker(self):
+    #     if self.opponents_used_dynamite < 100:
+    #         self.opponents_used_dynamite = self.opponents_used_dynamite + 1
 
-    def opponent_dynamite_tracker(self):
-        if self.opponents_used_dynamite < 100:
-            self.opponents_used_dynamite = self.opponents_used_dynamite + 1
+    def least_frequent_move(self):
+        str(self.opponent_used_moves)
